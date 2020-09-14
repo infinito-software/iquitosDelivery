e0db557f8cf69c645e8e6996115701144db5f435
infinito-software <65289762+infinito-software@users.noreply.github.com>
2020-09-13 22:11:12 -0500

    detalle peiddo arreglo

--- routes/index.js
+++ routes/index.js
@@ -2691,8 +2691,8 @@ router.post('/ActualizarPedido', jwtMW, async (req, res, next) => {
                     order_detail[i]["foodQuantity"],
                     order_detail[i]["foodPrice"],
                     order_detail[i]["foodSize"],
-                    parseFloat(order_detail[i]["foodExtraPrice"],
-                    order_detail[i]["foodAddon"]))
+                    parseFloat(order_detail[i]["foodExtraPrice"]),
+                    order_detail[i]["foodAddon"])
             }
 
             const request = pool.request()
